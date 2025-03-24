c9fb2665b4e5f360f753fff24f8a0dc88510d17a
vmilka <narah.1780@gmail.com>
2025-03-24 16:39:33 +0100

    done

--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,16 @@
-// UNCOMMENT THE CODE BELOW TO START
-
-/*
-
-describe("Iteration 3", () => {
-    describe("Function - calculateArea", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
-    })    
-})
-
-*/
+describe("calculateArea", function() {
+    it("should be defined", function() {
+      expect(calculateArea).toBeDefined();
+    });
+  
+    it("should return the area of a rectangle (product of two numbers)", function() {
+      expect(calculateArea(5, 10)).toBe(50);
+      expect(calculateArea(7, 3)).toBe(21);
+    });
+  
+    it("should return undefined if any argument is missing", function() {
+      expect(calculateArea(5)).toBeUndefined();
+      expect(calculateArea()).toBeUndefined();
+    });
+  });
+  
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,16 @@
-// UNCOMMENT THE CODE BELOW TO START
-
-/*
-
-describe("Iteration 2", () => {
-    describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
-    })    
-})
-
-*/
+describe("divide", function() {
+    it("should be defined", function() {
+      expect(divide).toBeDefined();
+    });
+  
+    it("should return the division of two numbers", function() {
+      expect(divide(6, 3)).toBe(2);
+      expect(divide(10, 2)).toBe(5);
+    });
+  
+    it("should return undefined if any argument is missing", function() {
+      expect(divide(6)).toBeUndefined();
+      expect(divide()).toBeUndefined();
+    });
+  });
+  
